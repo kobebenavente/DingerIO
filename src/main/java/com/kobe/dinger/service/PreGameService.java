2191fe95640e1b6c801c7634342bc91d55e9088f
kbenavente24 <kobeb@uw.edu>
2026-06-23 22:57:32 -0700

    Fixed pre-mature flag setting bug in PreGameService

--- src/main/java/com/kobe/dinger/service/PreGameService.java
+++ src/main/java/com/kobe/dinger/service/PreGameService.java
@@ -14,74 +14,41 @@ import com.kobe.dinger.model.GameState;
 import com.kobe.dinger.model.NotificationEvent;
 import com.kobe.dinger.model.Team;
 import com.kobe.dinger.model.TeamSubscription;
-import com.kobe.dinger.repository.TeamRepository;
 
 @Service
 public class PreGameService{
     private final NotificationService notificationService;
 
-    public PreGameService(NotificationService notificationService, TeamRepository teamRepository){
+    public PreGameService(NotificationService notificationService){
         this.notificationService = notificationService;
     }
 
     public void processGame(GameDTO gameInfo, Integer gamePk, List<TeamSubscription> subscriptions, GameState lastGameState, Team homeTeam, Team awayTeam){
-        for(TeamSubscription sub: subscriptions){
+        ZonedDateTime gameTime = Instant.parse(gameInfo.getGameDate()).atZone(ZoneId.of("America/Los_Angeles"));
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        long minutesUntilGame = ChronoUnit.MINUTES.between(now, gameTime);
+
+        boolean shouldSendGameStarting = !lastGameState.isGameStartingNotificationSent() && minutesUntilGame <= 2 && minutesUntilGame > 0;
+        boolean shouldSendGameDayReminder = !lastGameState.isGameStartingSoonNotificationSent() && minutesUntilGame <= 180 && minutesUntilGame > 60;
+
+        if (shouldSendGameStarting) lastGameState.setGameStartingNotificationSent(true);
+        if (shouldSendGameDayReminder) lastGameState.setGameStartingSoonNotificationSent(true);
+
+        for (TeamSubscription sub : subscriptions) {
             boolean subbedTeamIsHomeTeam = sub.getTeam().equals(homeTeam);
             Set<NotificationEvent> subNotificationEvents = sub.getNotificationEvents();
 
-            if(subNotificationEvents.contains(NotificationEvent.GAME_STARTING) &&
-            lastGameState != null && !lastGameState.isGameStartingNotificationSent()){
-
-                ZonedDateTime gameTime = Instant.parse(gameInfo.getGameDate())
-                .atZone(ZoneId.of("America/Los_Angeles"));
-
-                ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
-
-                long minutesUntilGame = ChronoUnit.MINUTES.between(now, gameTime);
-
-                if (minutesUntilGame <= 2 && minutesUntilGame > 0) {
-                    StringBuilder stringToSend = new StringBuilder();
-
-                    if(subbedTeamIsHomeTeam){
-                        stringToSend.append(sub.getTeam().getTeamName() + " vs " + awayTeam.getTeamName()
-                        + " is now starting");
-                    } else {
-                        stringToSend.append(sub.getTeam().getTeamName() + " vs " + homeTeam.getTeamName()
-                        + " is now starting");
-                    }
-
-                    lastGameState.setGameStartingNotificationSent(true);
-                    notificationService.sendNotification(sub, stringToSend.toString());
-                }
+            if (shouldSendGameStarting && subNotificationEvents.contains(NotificationEvent.GAME_STARTING)) {
+                String opponent = subbedTeamIsHomeTeam ? awayTeam.getTeamName() : homeTeam.getTeamName();
+                notificationService.sendNotification(sub, sub.getTeam().getTeamName() + " vs " + opponent + " is now starting");
             }
 
-            if(subNotificationEvents.contains(NotificationEvent.GAME_DAY_REMINDER) &&
-            lastGameState != null && !lastGameState.isGameStartingSoonNotificationSent()){
-                ZonedDateTime gameTime = Instant.parse(gameInfo.getGameDate())
-                .atZone(ZoneId.of("America/Los_Angeles"));
-
-                ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
-
-                long minutesUntilGame = ChronoUnit.MINUTES.between(now, gameTime);
-
-                    if (minutesUntilGame <= 180 && minutesUntilGame > 60) {
-                    StringBuilder stringToSend = new StringBuilder();
-
-                    java.time.format.DateTimeFormatter timeFormatter = java.time.format.DateTimeFormatter.ofPattern("h:mm a");
-                    String pst = gameTime.withZoneSameInstant(ZoneId.of("America/Los_Angeles")).format(timeFormatter) + " PST";
-                    String est = gameTime.withZoneSameInstant(ZoneId.of("America/New_York")).format(timeFormatter) + " EST";
-
-                    if(subbedTeamIsHomeTeam){
-                        stringToSend.append(sub.getTeam().getTeamName() + " play the " + awayTeam.getTeamName()
-                        + " today at " + pst + " / " + est);
-                    } else {
-                        stringToSend.append(sub.getTeam().getTeamName() + " play the " + homeTeam.getTeamName()
-                        + " today at " + pst + " / " + est);
-                    }
-
-                    lastGameState.setGameStartingSoonNotificationSent(true);
-                    notificationService.sendNotification(sub, stringToSend.toString());
-                }                
+            if (shouldSendGameDayReminder && subNotificationEvents.contains(NotificationEvent.GAME_DAY_REMINDER)) {
+                java.time.format.DateTimeFormatter timeFormatter = java.time.format.DateTimeFormatter.ofPattern("h:mm a");
+                String pst = gameTime.withZoneSameInstant(ZoneId.of("America/Los_Angeles")).format(timeFormatter) + " PST";
+                String est = gameTime.withZoneSameInstant(ZoneId.of("America/New_York")).format(timeFormatter) + " EST";
+                String opponent = subbedTeamIsHomeTeam ? awayTeam.getTeamName() : homeTeam.getTeamName();
+                notificationService.sendNotification(sub, sub.getTeam().getTeamName() + " play the " + opponent + " today at " + pst + " / " + est);
             }
         }
     }
